e278d3e1a59465b84c4be9cec0baee4812d3bcf1
Henry Richardson <henry.d.richardson@gmail.com>
2024-07-30 09:58:49 -0400

    Adding keymap for non-printable mode

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- LazyVim.toggle.map("<leader>un", LazyVim.toggle("something", { name = "Non-Printables" }))
+
+-- map("n", "<leader>un", "something", { desc = "Display Non-Printables" })
+--
+local wk = require("which-key")
+
+wk.add({
+  {
+    "<leader>uv",
+    icon = function()
+      return vim.g.non_printable_mode == 0 and { icon = "\u{f204} ", color = "yellow" }
+        or { icon = "\u{f205} ", color = "green" }
+    end,
+    "<cmd>:ToggleNonPrintable<CR>",
+    desc = function()
+      return (vim.g.non_printable_mode == 0 and "Enable" or "Disable") .. " Non-Printable"
+    end,
+    mode = "n",
+  },
+})
