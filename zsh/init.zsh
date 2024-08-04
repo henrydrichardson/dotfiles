0dabd5dcbfcff25a5cd6d70884b1c551d11a9972
Henry Richardson <henry.d.richardson@gmail.com>
2024-08-04 13:32:34 -0400

    Seperate oh-my-zsh-loader, adding zsh-eza to overwrite ls alias

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -5,40 +5,11 @@ local working_directory="${funcsourcetrace[1]%/*}"
 export XDG_CONFIG_HOME="$HOME/.config"
 export LG_CONFIG_FILE="$XDG_CONFIG_HOME/lazygit/config.yml,$HOME/.local/share/nvim/lazy/tokyonight.nvim/extras/lazygit/tokyonight_moon.yml"
 
+# oh-my-zsh Configuration
+source "$working_directory/oh-my-zsh-loader.zsh"
+
 # eza Configuration
 source "$working_directory/eza_configuration.zsh"
 
-FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-
-ZSH_THEME="powerlevel10k/powerlevel10k"
-
-plugins=(
-  aws
-  docker
-  docker-compose
-  fzf
-  git
-  git-prompt
-  gitfast
-  helm
-  kubectl
-  minikube
-  pyenv
-  rbenv
-  you-should-use
-  zsh-autosuggestions
-  zsh-bat
-  zsh-syntax-highlighting
-)
-
+# Custom Alias
 alias vim="nvim"
-
-# Update Frequency Settings
-zstyle ':omz:update' mode auto
-zstyle ':omz:update' frequency 7
-
-# ZSH Completion Settings
-LISTMAX=200
-
-eval "$(pyenv init --path)"
-eval "$(zoxide init zsh)"
