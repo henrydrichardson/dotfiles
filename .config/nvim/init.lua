ccd85f8da046d4455e7cd2b58951a49c9688ee8c
Henry Richardson <henry.d.richardson@gmail.com>
2024-08-19 10:26:44 -0400

    Moving neovim lsp log files to tmp dir.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,32 @@
+-- TODO put this function in a better place.
+local function link_lsp_log_to_temporary_file(destination)
+  if vim.fn.filereadable(destination) == 0 and vim.fn.isdirectory(destination) then
+    local result = vim.fn.system({ "ln", "-s", vim.lsp.get_log_path(), destination })
+    if vim.v.shell_error ~= 0 then
+      vim.notify("Failed to create symlink: " .. result, vim.log.levels.ERROR)
+    end
+  end
+end
+
+local function create_temporary_lsp_log_file()
+  local log_dir = "/tmp/neovim"
+  local lsp_log_file = log_dir .. "/lsp.log"
+
+  vim.fn.mkdir(log_dir, "p")
+
+  local file = io.open(lsp_log_file, "a")
+  if file then
+    file:close()
+    link_lsp_log_to_temporary_file(lsp_log_file)
+  else
+    vim.notify("Failed to create the lsp log file: " .. lsp_log_file, vim.log.levels.ERROR)
+  end
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = create_temporary_lsp_log_file,
+})
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
