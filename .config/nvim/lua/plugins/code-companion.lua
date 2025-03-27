46d814da1fa6039b095049a24a390fe4ec0474d0
Henry Richardson <henry.d.richardson@gmail.com>
2025-03-27 11:14:10 -0400

    Add Keymaps for Code Companion

--- .config/nvim/lua/plugins/code-companion.lua
+++ .config/nvim/lua/plugins/code-companion.lua
@@ -24,4 +24,16 @@ return {
       },
     },
   },
+  keys = {
+    -- Normal Mode Operations
+    { "<leader>aa", "<cmd>:CodeCompanionActions<CR>", mode = { "n", "v" }, desc = "Launch AI" },
+    { "<leader>ac", "<cmd>:CodeCompanionChat<CR>", mode = { "n", "v" }, desc = "Chat with AI" },
+    { "<leader>ah", "<cmd>:CodeCompanionChat Toggle<CR>", mode = { "n", "v" }, desc = "Toggle AI Chat Window" },
+
+    -- Visual Mode Operations (Functions had to be seprated to retain replace/limited context operations)
+    { "<leader>ae", ":'<,'>CodeCompanionChat /explain<CR>", mode = "v", desc = "Explain Code with AI" },
+    { "<leader>af", ":'<,'>CodeCompanionChat /fix<CR>", mode = "v", desc = "Fix Code with AI" },
+    { "<leader>al", ":'<,'>CodeCompanionChat /lsp<CR>", mode = "v", desc = "Explain LSP Diagnoses with AI" },
+    { "<leader>at", ":'<,'>CodeCompanionChat /tests<CR>", mode = "v", desc = "Generate Tests with AI" },
+  },
 }
