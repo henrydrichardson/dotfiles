052796b0664beec21caed1009275a9978884fed2
Henry Richardson <henry.d.richardson@gmail.com>
2025-05-25 22:07:18 -0400

    refactor: remove eza configuration
    This commit removes the sourcing of the eza configuration file.

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -4,6 +4,3 @@ local working_directory="${funcsourcetrace[1]%/*}"
 # LazyGit Configuration
 export XDG_CONFIG_HOME="$HOME/.config"
 export LG_CONFIG_FILE="$XDG_CONFIG_HOME/lazygit/config.yml,$HOME/.local/share/nvim/lazy/tokyonight.nvim/extras/lazygit/tokyonight_moon.yml"
-
-# eza Configuration
-source "$working_directory/eza_configuration.zsh"
